efc471b062902fb372491e9d2acc67acd68e2866
Dungweb <116371263+Dungweb@users.noreply.github.com>
2023-12-08 07:36:34 +0700

    commit

--- Presenters/StatisticsPresenter.cs
+++ Presenters/StatisticsPresenter.cs
@@ -69,34 +69,24 @@ namespace BachHoaXanh.Presenters
                 amount.Add(y);
             }
             //labelText[2].Text = "THỐNG KÊ THEO THÁNG TRONG NĂM " + currentYear;
-            //statisticDay.Controls.Add(labelText[2], BorderLayout.NORTH);
+            for (int i = 1; i <= currentMonth; i++)
+            {
+                List<List<string>> result = repository.ExcuteQuerry("SELECT SUM(`import`.total) " +
+                            "FROM `import` " +
+                            "WHERE YEAR(`import`.received_date) = " + currentYear + " AND MONTH(`import`.received_date) = " + i);
+                if (result.Count > 0)
+                {
+                    expenses.Add(Double.Parse(result[0][0].Split("\\.")[0]));
+                }
 
-            //for (int i = 1; i <= currentMonth; i++)
-            //{
-            //    try
-            //    {
-            //        List<List<string>> inventory = MySQL.executeQueryStatistic("SELECT SUM(`import`.total) " +
-            //            "FROM `import` " +
-            //            "WHERE YEAR(`import`.received_date) = " + currentYear + " AND MONTH(`import`.received_date) = " + i);
-            //        expenses.Add(double.Parse(inventory[0][0].Split('.')[0]));
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        throw new RuntimeException(ex);
-            //    }
-
-            //    try
-            //    {
-            //        List<List<string>> inventory = MySQL.executeQueryStatistic("SELECT SUM(`receipt`.total) " +
-            //            "FROM `receipt` " +
-            //            "WHERE YEAR(`receipt`.invoice_date) = " + currentYear + " AND MONTH(`receipt`.invoice_date) = " + i);
-            //        amount.Add(double.Parse(inventory[0][0].Split('.')[0]));
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        throw new RuntimeException(ex);
-            //    }
-            //}
+                result = repository.ExcuteQuerry("SELECT SUM(`receipt`.total) " +
+                            "FROM `receipt` " +
+                            "WHERE YEAR(`receipt`.invoice_date) = " + currentYear + " AND MONTH(`receipt`.invoice_date) = " + i);
+                if (result.Count > 0)
+                {
+                    amount.Add(Double.Parse(result[0][0].Split("\\.")[0])); ;
+                }
+            }
 
             view.GetChart3.Clear();
             view.GetChart3.AddData(new ModelChart("January", new double[] { expenses[0], amount[0], amount[0] - expenses[0] }));
@@ -133,35 +123,27 @@ namespace BachHoaXanh.Presenters
                 amount.Add(y);
             }
             //labelText[1].setText("THỐNG KÊ THEO QUÝ TRONG NĂM " + currentYear);
-            //statisticMonth.add(labelText[1], BorderLayout.NORTH);
-            //for (int i = 1; i <= currentQuarter; i++)
-            //{
-            //    XYChart.Series<String, Number> series1 = new XYChart.Series<>();
-            //    series1.setName("Quý " + i);
-            //    try
-            //    {
-            //        List<List<String>> inventory = MySQL.executeQueryStatistic("SELECT SUM(`import`.total) " +
-            //            "FROM `import` " +
-            //            "WHERE YEAR(`import`.received_date) = " + currentYear + " AND QUARTER(`import`.received_date) = " + i);
-            //        expenses.add(Double.parseDouble(inventory.get(0).get(0).split("\\.")[0]));
+            for (int i = 1; i <= currentQuarter; i++)
+            {
+                //    XYChart.Series<String, Number> series1 = new XYChart.Series<>();
+                //    series1.setName("Quý " + i);
 
+                List<List<string>> result = repository.ExcuteQuerry("SELECT SUM(`import`.total) " +
+                            "FROM `import` " +
+                            "WHERE YEAR(`import`.received_date) = " + currentYear + " AND QUARTER(`import`.received_date) = " + i);
+                if (result.Count > 0)
+                {
+                    expenses.Add(Double.Parse(result[0][0].Split("\\.")[0]));
+                }
 
-            //    }
-            //    catch (SQLException | IOException ex) {
-            //    throw new RuntimeException(ex);
-            //}
-
-            //try
-            //{
-            //    List<List<String>> inventory = MySQL.executeQueryStatistic("SELECT SUM(`receipt`.total) " +
-            //        "FROM `receipt` " +
-            //        "WHERE YEAR(`receipt`.invoice_date) = " + currentYear + " AND QUARTER(`receipt`.invoice_date) = " + i);
-            //    amount.add(Double.parseDouble(inventory.get(0).get(0).split("\\.")[0]));
-
-            //}
-            //catch (SQLException | IOException ex) {
-            //    throw new RuntimeException(ex);
-            //}
+                result = repository.ExcuteQuerry("SELECT SUM(`receipt`.total) " +
+                        "FROM `receipt` " +
+                        "WHERE YEAR(`receipt`.invoice_date) = " + currentYear + " AND QUARTER(`receipt`.invoice_date) = " + i);
+                if (result.Count > 0)
+                {
+                    amount.Add(Double.Parse(result[0][0].Split("\\.")[0])); ;
+                }
+            }
             view.GetChart2.Clear();
             view.GetChart2.AddData(new ModelChart("The First Quarter", new double[] { expenses[0], amount[0], amount[0] - expenses[0] }));
             view.GetChart2.AddData(new ModelChart("Second Quarter", new double[] { expenses[1], amount[1], amount[1] - expenses[1] }));
@@ -178,7 +160,6 @@ namespace BachHoaXanh.Presenters
             expenses = new List<double>();
             amount = new List<double>();
             //labelText[0].setText("THỐNG KÊ THEO 3 NĂM GẦN NHẤT ");
-            //statisticYear.add(labelText[0], BorderLayout.NORTH);
             view.GetChart1.Clear();
             currentYear = DateTime.Now.Year;
             for (int i = currentYear - 2; i <= currentYear; i++)
